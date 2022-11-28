2c45a175cff593d61994066a56f9f1ccd669e93b
wuang <wuangmai@gmail.com>
2022-11-28 02:43:45 +0100

    wtf is that?

---
+++ Api/Data/UsersDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+
+public class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
